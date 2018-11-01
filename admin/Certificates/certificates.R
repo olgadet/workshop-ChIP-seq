df2f13c48587eac0de63729efaab9d822c9e9456
olgadet <olga.dethlefsen@nbis.se>
2018-11-01 18:08:18 +0100

    Adding certificates, attendance list and participants list

---
+++ admin/Certificates/certificates.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+library(knitr)
+
+# Input data
+
+file.csv <- "NBIS-ChIP-seq-participants-2018.csv"
+data.csv <- read.csv(file.csv)
+ 
+# Number
+idx.final <- which(data.csv$Final==1)
+
+studentsID <- 1:length(idx.final)
+#studentsID <- 1:3
+
+for (st in studentsID){
+  print(st)
+  knit2pdf("certificates.Rnw", output=paste0('diploma_', st, '.tex'))
+}
+
+
+for (st in studentsID){
+  print(st)
+  tools::texi2pdf(paste0("/Users/olga/Desktop/ChIP-seq/admin/Certificates/Out/", paste0('diploma_', st, '.tex')),
+                  clean = TRUE, quiet = TRUE)
+}
+
+#files.remove <- c(dir(pattern=".out"), dir(pattern=".aux"), dir(pattern=".bbl"), dir(pattern="xml"), dir(pattern=".bib"), dir(pattern=".log"), dir(pattern="blg"), dir(pattern=".tex"))
+#file.remove(files.remove)
